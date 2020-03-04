066947faa45cb59e853616e8577ab040ef29d2d0
Ali Rizvi-Santiago <arizvisa@gmail.com>
2020-03-03 19:03:02 -0600

    Added some comments to the bin/fuzz.sh script.

--- bin/fuzz.sh
+++ bin/fuzz.sh
@@ -2,6 +2,7 @@
 arg0="$0"
 target="$1"
 
+## Set some default parameters and validate what we received
 run_args="-verbosity=1 -runs=-1 -print_final_stats=1"
 fork_args="-fork=1 -ignore_ooms=1 -ignore_timeouts=1 -ignore_crashes=1 -detect_leaks=1"
 
@@ -10,14 +11,17 @@ if [ $# -lt 1 ]; then
     exit 0
 fi
 
-if [ ! -x "$target" ]; then
+## Figure out the actual target and make sure that it's well-formed
+p=`dirname "$target"`
+name=`basename "$target"`
+
+if [ ! -x "$p/$name.fuzzer" ]; then
     printf "%s: Unable to run requested executable: %s\n" "$arg0" "$target"
     exit 1
 fi
 shift
 
-p=`dirname "$target"`
-name=`basename "$target" .fuzzer`
+## Identify the corpus and artifact directoriees and make sure they exist
 corpus="$p/$name.corpus"
 artifacts="$p/$name.crash"
 
@@ -31,4 +35,5 @@ if [ ! -d "$artifacts" ]; then
     exit 1
 fi
 
+## Set it off
 exec "$p/$name.fuzzer" $run_args $fork_args "-artifact_prefix=$artifacts/" "$corpus" "$@"
