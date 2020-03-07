9cef8145247c1a97ecdef3c4bebee3c7d6f19ef8
Ali Rizvi-Santiago <arizvisa@gmail.com>
2020-03-06 18:42:15 -0600

    Updated the bin/minimize-testcase.sh script to lock itself to directories relative to the target fuzzer.

--- bin/minimize-testcase.sh
+++ bin/minimize-testcase.sh
@@ -14,6 +14,21 @@ if [ $# -lt 3 ]; then
 fi
 shift 3
 
+## Identify the artifact and output directory and make sure they exist
+artifacts="$p/$name.crash"
+output="$p/$name.output"
+
+if [ ! -d "$artifacts" ]; then
+    printf "%s: Artifact directory does not exist: %s\n" "$arg0" "$artifacts"
+    exit 1
+fi
+
+if [ ! -d "$output" ]; then
+    printf "%s: Output directory for minimization does not exist: %s\n" "$arg0" "$output"
+    exit 1
+fi
+
+
 ## Figure out the actual target and make sure that it's well-formed
 p=`dirname "$target"`
 name=`basename "$target"`
@@ -24,8 +39,8 @@ if [ ! -x "$p/$name.fuzzer" ]; then
 fi
 
 ## Validate the count and verify that the testcase exists
-if [ ! -f "$testcase" ]; then
-    printf "%s: The specified testcase does not exist: %s\n" "$arg0" "$testcase"
+if [ ! -f "$artifacts/$testcase" ]; then
+    printf "%s: The specified testcase does not exist (%s): %s\n" "$arg0" "$testcase" "$artifacts/$testcase"
     exit 1
 fi
 
@@ -44,4 +59,4 @@ fi
 ulimit -v unlimited
 
 ## Set it off
-exec "$p/$name.fuzzer" $run_args $minimize_args "-runs=$count" "$testcase" "$@"
+exec "$p/$name.fuzzer" $run_args $minimize_args "--artifact_prefix=$output/" "-runs=$count" "$artifacts/$testcase" "$@"
